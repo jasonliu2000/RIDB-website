33140d86f639c1bd10cdccfca01b69ecde6d844a
Jason Liu <jasonliu00@gmail.com>
2023-03-08 21:48:11 -0500

    Display Recreational Area names

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [recAreas, setRecAreas] = useState([]);
+
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_RIDB_BASE_URL}/recareas/10175174/links?limit=50&offset=0&apikey=${process.env.REACT_APP_RIDB_API_KEY}`, {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
+    fetch(`${process.env.REACT_APP_RIDB_BASE_URL}/recareas?limit=50&offset=0&apikey=${process.env.REACT_APP_RIDB_API_KEY}`, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.RECDATA);
+        setRecAreas(data.RECDATA);
       }
-    }).then(response => 
-      response.json()
-    ).then(data => 
-      console.log(data)
-    )
+    );
   }, []);
 
   return (
     <div>
     <p>RIDB - hello world</p>
+    <p>Showing 50 rec areas:</p>
+    <ul>
+      {recAreas.map( recArea => <li key={recArea.RecAreaID}>{recArea.RecAreaName}</li> )}
+    </ul>
   </div>
   );
 }
