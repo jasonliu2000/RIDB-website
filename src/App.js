82e6782d18763edbfa15a9f4e54d41fff2140f9d
Jason Liu <jasonliu00@gmail.com>
2023-03-08 21:22:41 -0500

    Configure calls to RIDB API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_RIDB_BASE_URL}/recareas/10175174/links?limit=50&offset=0&apikey=${process.env.REACT_APP_RIDB_API_KEY}`, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    }).then(response => 
+      response.json()
+    ).then(data => 
+      console.log(data)
+    )
+  }, []);
+
   return (
     <div>
     <p>RIDB - hello world</p>
